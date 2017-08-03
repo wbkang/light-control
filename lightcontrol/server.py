58d3e0712a35052d0016fa3c3b3ffda1ba56b305
Woongbin Kang <wbk@outlook.com>
2017-08-02 21:30:12 -0400

    Add some locks

--- lightcontrol/server.py
+++ lightcontrol/server.py
@@ -4,6 +4,7 @@ import RPi.GPIO as GPIO
 import time
 import threading
 import logging
+from threading import RLock, Lock
 from tzlocal import get_localzone
 from flask import Flask, render_template, url_for, request, make_response
 from lightcontrol.config import lights
@@ -23,33 +24,40 @@ home_dir = expanduser('~')
 class Preferences:
     def __init__(self, filename):
         self.filename = filename
+        self.lock = RLock()
     def read(self):
-        if os.path.exists(self.filename):
-            try:
-                with open(self.filename, 'rb') as f:
-                    return json.loads(f.read().decode('utf-8'))
-            except:
-                logger.exception("Error reading JSON. Resetting preferences")
+        with self.lock:
+            if os.path.exists(self.filename):
+                try:
+                    with open(self.filename, 'rb') as f:
+                        return json.loads(f.read().decode('utf-8'))
+                except:
+                    logger.exception("Error reading JSON. Resetting preferences")
+                    return dict()
+            else:
                 return dict()
-        else:
-            return dict()
     def write(self, d):
-        with open(self.filename, 'wb') as f:
-            return f.write(json.dumps(d).encode('utf-8'))
+        with self.lock:
+            with open(self.filename, 'wb') as f:
+                return f.write(json.dumps(d).encode('utf-8'))
     def update(self, key, value):
-        p = self.read()
-        p[key] = value
-        self.write(p)
+        with self.lock:
+            p = self.read()
+            p[key] = value
+            self.write(p)
 
 pref = Preferences(filename=home_dir + '/.lightcontrol')
 
+switch_lock = Lock()
+
 def toggle_switch(light_name, onoff):
-    line = lights[light_name][0 if onoff else 1]
-    GPIO.setup(line, GPIO.OUT)
-    GPIO.output(line, GPIO.HIGH)
-    time.sleep(0.5)
-    GPIO.output(line, GPIO.LOW)
-    pref.update(light_name, onoff)
+    with switch_lock:
+        pref.update(light_name, onoff)
+        line = lights[light_name][0 if onoff else 1]
+        GPIO.setup(line, GPIO.OUT)
+        GPIO.output(line, GPIO.HIGH)
+        time.sleep(0.5)
+        GPIO.output(line, GPIO.LOW)
 
 @app.route("/")
 def index():
